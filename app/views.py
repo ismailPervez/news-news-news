fcc586ec6b1f25a060cc3c31f4b8327075b6eb90
ismailpervez <freelanceismail001@gmail.com>
2021-09-12 15:16:26 +0300

    user is now able to choose between different sources

--- app/views.py
+++ app/views.py
@@ -1,18 +1,28 @@
 from flask import render_template
 from app import app
-from .request import get_news
+from .request import get_news, get_sources
 
 """
 Home page - the route that loads when the page first renders
 """
 @app.route("/")
 def index():
-    news_source = 'bbc-news'
-    news = get_news('top-headlines', news_source)
+    all_sources = get_sources()
+    all_sources = all_sources["sources"]
+    news = get_news('top-headlines', 'bbc-news')
     if (news['status'] == 'ok'):
         news_articles = news["articles"]
-    return render_template('home.html', articles=news_articles)
+    return render_template('home.html', articles=news_articles, sources=all_sources)
 
 """
-
+Different news sources route
 """
+@app.route("/<source>")
+def news_source_articles(source):
+    all_sources = get_sources()
+    all_sources = all_sources["sources"]
+    news = get_news('top-headlines', source)
+    if (news['status'] == 'ok'):
+        news_articles = news['articles']
+
+    return render_template('source.html', articles=news_articles, sources=all_sources)        
